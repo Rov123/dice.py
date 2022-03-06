845c314c11e991a0046744031e3d06f021726d9d
Rov123 <74053186+Rov123@users.noreply.github.com>
2022-03-06 12:26:31 +0000

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,15 @@
+import random 
+
+def roll_dice (dice_type , dice-amount}:
+             
+              dice-rolls = []
+               while dice-amount !=0
+                dice-rolls = dice-rolls + random.randrange(1,dice_type)
+                dice-amount -=1  
+              return dice-rolls
+               
+roll_dice(20,100)
+               
+               
+               
+               
